a333f5f0d4f2bb124ddc775b93ec07139a51ab2b
creep <creeper0809@hotmail.com>
2025-07-03 16:50:06 +0900

    feat. 기본설정

--- core/config.py
+++ core/config.py
@@ -7,16 +7,16 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "quokkalib"
     DEBUG: bool = True
     GEMINI_API_KEY: str
-    DATABASE_URL: str = "sqlite:///./database.db"
+    DATABASE_URL: str
 
-    SECRET_KEY :str = "your_secret_key"
-    ALGORITHM :str = "HS256"
+    SECRET_KEY :str
+    ALGORITHM :str
     EXPIRE_TIME : int = 30                          # 분 단위
 
     GEMINI_MODEL: str = "gemini-2.0-flash"
 
     class Config:
-        env_file = ".env"  # .env 파일에서 값 불러오기
+        env_file = ".env"
 
 @lru_cache
 def get_settings() -> Settings:
