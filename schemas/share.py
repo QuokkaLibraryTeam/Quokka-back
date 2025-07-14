07981d1049d82e5634aa3b0e7cff853d7a106f59
kdu <binaryk01@naver.com>
2025-07-14 19:55:55 +0900

    fix: share API

--- schemas/share.py
+++ schemas/share.py
@@ -1,20 +1,22 @@
-# schemas/share.py
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 from typing import List
 
-from pydantic import BaseModel, ConfigDict
 from schemas.story import StoryOutWithDetail
-
 
 class ShareCreate(BaseModel):
     tags: List[str]
 
     model_config = ConfigDict(from_attributes=True)
 
+class TagsUpdate(BaseModel):
+    tags: List[str]
+
+    model_config = ConfigDict(from_attributes=True)
 
 class ShareOut(BaseModel):
     id: int
-    story: StoryOutWithDetail   # 퍼블리시된 동화 상세
+    story: StoryOutWithDetail
     tags: List[str]
     created_at: datetime
 
